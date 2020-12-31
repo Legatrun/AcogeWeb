db911e8e7c8a9cf1b9ca091f092fbd28778f3dcd
Roger Ramos Mayta <roger.ramos@alphasys.com.bo>
2020-12-31 02:36:35 -0400

    Monedas Validacion

--- src/components/Operaciones/Monedas/Monedas.ts
+++ src/components/Operaciones/Monedas/Monedas.ts
@@ -13,7 +13,7 @@ export default class AdmMonedasComponent extends Vue {
 		{ text: 'descripcion', align: 'left', sortable: false, value: 'descripcion', width: '15%' },
 		{ text: 'sigla', align: 'left', sortable: false, value: 'sigla', width: '15%' },
 		{ text: 'monedalocal', align: 'left', sortable: false, value: 'monedalocal', width: '15%' },
-		{ text: 'Operaciones', align: 'center', sortable: false, value: 'action', width: '20%' },
+		{ text: 'Operaciones', align: 'left', sortable: false, value: 'action', width: '20%' },
 	];
 	private WebApi = new services.Endpoints();
 
@@ -24,6 +24,11 @@ export default class AdmMonedasComponent extends Vue {
 	private operacion = '';
 	private helper: helpers = new helpers();
 	private popup = new popup.Swal();
+	private activo = false;
+	validacion = [
+		(v: any) => !!v || 'El campo es requerido',
+    (v: any) => !/^\s*$/.test(v) || 'No se permite espacios vacios',
+  ];
 	private FormatDate(data: any) {
 		return moment(data).format('YYYY-MM-DD');
 	}
@@ -111,7 +116,7 @@ export default class AdmMonedasComponent extends Vue {
 	private Eliminar(data: services.clase_monedas): void {
 		swal.fire({
 			title: 'Esta seguro de esta operacion?',
-			text: 'Eliminacion de Registro' + data.idmoneda,
+			text: 'Eliminacion de Registro ' + data.descripcion,
 			type: 'warning',
 			showCancelButton: true,
 			confirmButtonColor: 'green',
