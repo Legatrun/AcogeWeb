4fbad3f6a03cd9a0d8dfc052285d33fef7052f91
Roger Ramos Mayta <roger.ramos@alphasys.com.bo>
2020-12-31 02:57:57 -0400

    TipodePagos Diseño

--- src/components/Operaciones/TipodePagos/TipodePagos.vue
+++ src/components/Operaciones/TipodePagos/TipodePagos.vue
@@ -32,13 +32,13 @@
 							<template v-slot:activator="{ on }">
 								<v-btn color="success" v-on="on" fab small dark  @click="Actualizar(props.item)"><v-icon>edit</v-icon></v-btn>
 							</template>
-							<span>Modificar Registro de Demo</span>
+							<span>Modificar Registro de Tipo de Pago</span>
 						</v-tooltip>
 						<v-tooltip style="padding-left:10px" bottom>
 							<template v-slot:activator="{ on }" >
 								<v-btn color="error" v-on="on" fab small dark  @click="Eliminar(props.item)"><v-icon>delete</v-icon></v-btn>
 							</template>
-							<span>Eliminar Registro de Demo</span>
+							<span>Eliminar Registro de Tipo de Pago</span>
 						</v-tooltip>
 					</td>
 				</tr>
@@ -48,7 +48,7 @@
 					<template v-slot:activator="{ on }">
 						<v-btn color="accent" v-on="on" @click="Insertar()">Adicionar Nuevo Registro de TipodePagos</v-btn>
 					</template>
-					<span>Adicionar nuevo registro de cliente</span>
+					<span>Adicionar nuevo registro de Tipo de Pago</span>
 				</v-tooltip>
 			</template>
 			<template v-slot:no-data>
@@ -63,7 +63,7 @@
 					<v-toolbar-title>Datos de TipodePagos</v-toolbar-title>
 				</v-toolbar>
 				<v-divider></v-divider>
-				<v-form ref="form" style="padding:10px">
+				<v-form ref="form" style="padding:10px" v-model="activo">
 					<v-card-text>
 						<v-layout wrap>
 							<template v-if="operacion == 'Insert'">
@@ -75,6 +75,7 @@
 												clearable
 												persistent-hint
 												required
+												:rules="validacion"
 												@input="tipodepagos.idtipopago = updateText(tipodepagos.idtipopago)">
 									</v-text-field>
 								</v-flex>
@@ -97,6 +98,7 @@
 											clearable
 											persistent-hint
 											required
+											:rules="validacion"
 											@input="tipodepagos.descripcion = updateText(tipodepagos.descripcion)">
 								</v-text-field>
 							</v-flex>
@@ -105,7 +107,7 @@
 				</v-form>
 				<v-divider></v-divider>
 				<v-card-actions style="justify-content: center;padding:10px">
-					<v-btn color="success" dark style="width: 50%" @click="Grabar()">Grabar</v-btn>
+					<v-btn color="success" dark style="width: 50%" :disabled="!activo" @click="Grabar()">Grabar</v-btn>
 					<v-btn color="error" dark style="width: 50%" @click="Cancelar()">Cancelar</v-btn>
 				</v-card-actions>
 			</v-card>
