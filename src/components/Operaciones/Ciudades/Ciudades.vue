ed706fc3f911ce68b016c7886a85263133e5c413
Roger Ramos Mayta <roger.ramos@alphasys.com.bo>
2020-12-31 01:41:44 -0400

    Ciudades Diseño

--- src/components/Operaciones/Ciudades/Ciudades.vue
+++ src/components/Operaciones/Ciudades/Ciudades.vue
@@ -35,13 +35,13 @@
 							<template v-slot:activator="{ on }">
 								<v-btn color="success" v-on="on" fab small dark  @click="Actualizar(props.item)"><v-icon>edit</v-icon></v-btn>
 							</template>
-							<span>Modificar Registro de Demo</span>
+							<span>Modificar Registro de Ciudad</span>
 						</v-tooltip>
 						<v-tooltip style="padding-left:10px" bottom>
 							<template v-slot:activator="{ on }" >
 								<v-btn color="error" v-on="on" fab small dark  @click="Eliminar(props.item)"><v-icon>delete</v-icon></v-btn>
 							</template>
-							<span>Eliminar Registro de Demo</span>
+							<span>Eliminar Registro de Ciudad</span>
 						</v-tooltip>
 					</td>
 				</tr>
@@ -51,7 +51,7 @@
 					<template v-slot:activator="{ on }">
 						<v-btn color="accent" v-on="on" @click="Insertar()">Adicionar Nuevo Registro de Ciudades</v-btn>
 					</template>
-					<span>Adicionar nuevo registro de cliente</span>
+					<span>Adicionar nuevo registro de Ciudad</span>
 				</v-tooltip>
 			</template>
 			<template v-slot:no-data>
@@ -66,7 +66,7 @@
 					<v-toolbar-title>Datos de Ciudades</v-toolbar-title>
 				</v-toolbar>
 				<v-divider></v-divider>
-				<v-form ref="form" style="padding:10px">
+				<v-form ref="form" style="padding:10px" v-model="activo">
 					<v-card-text>
 						<v-layout wrap>
 							<template v-if="operacion == 'Insert'">
@@ -78,6 +78,7 @@
 												clearable
 												persistent-hint
 												required
+												:rules="validacion"
 												@input="ciudades.idciudad = updateText(ciudades.idciudad)">
 									</v-text-field>
 								</v-flex>
@@ -100,6 +101,7 @@
 											clearable
 											persistent-hint
 											required
+											:rules="validacion"
 											@input="ciudades.idpais = updateText(ciudades.idpais)">
 								</v-text-field>
 							</v-flex>
@@ -111,6 +113,7 @@
 											clearable
 											persistent-hint
 											required
+											:rules="validacion"
 											@input="ciudades.descripcion = updateText(ciudades.descripcion)">
 								</v-text-field>
 							</v-flex>
@@ -122,6 +125,7 @@
 											clearable
 											persistent-hint
 											required
+											:rules="validacion"
 											@input="ciudades.sigla = updateText(ciudades.sigla)">
 								</v-text-field>
 							</v-flex>
@@ -133,6 +137,7 @@
 											clearable
 											persistent-hint
 											required
+											:rules="validacion"
 											@input="ciudades.idmoneda = updateText(ciudades.idmoneda)">
 								</v-text-field>
 							</v-flex>
@@ -141,7 +146,7 @@
 				</v-form>
 				<v-divider></v-divider>
 				<v-card-actions style="justify-content: center;padding:10px">
-					<v-btn color="success" dark style="width: 50%" @click="Grabar()">Grabar</v-btn>
+					<v-btn color="success" dark style="width: 50%" :disabled="!activo" @click="Grabar()">Grabar</v-btn>
 					<v-btn color="error" dark style="width: 50%" @click="Cancelar()">Cancelar</v-btn>
 				</v-card-actions>
 			</v-card>
