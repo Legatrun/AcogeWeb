6fc7783f06431ed6a12b6dd2ba60700b669b1376
Roger Ramos Mayta <roger.ramos@alphasys.com.bo>
2020-12-31 02:40:28 -0400

    Pais Validacion

--- src/components/Operaciones/Pais/Pais.ts
+++ src/components/Operaciones/Pais/Pais.ts
@@ -12,7 +12,7 @@ export default class AdmPaisComponent extends Vue {
 		{ text: 'IDPais', align: 'left', sortable: true, value: 'idpais', width: '15%' },
 		{ text: 'descripcion', align: 'left', sortable: false, value: 'descripcion', width: '15%' },
 		{ text: 'sigla', align: 'left', sortable: false, value: 'sigla', width: '15%' },
-		{ text: 'Operaciones', align: 'center', sortable: false, value: 'action', width: '20%' },
+		{ text: 'Operaciones', align: 'left', sortable: false, value: 'action', width: '20%' },
 	];
 	private WebApi = new services.Endpoints();
 
@@ -23,6 +23,11 @@ export default class AdmPaisComponent extends Vue {
 	private operacion = '';
 	private helper: helpers = new helpers();
 	private popup = new popup.Swal();
+	private activo = false;
+	validacion = [
+		(v: any) => !!v || 'El campo es requerido',
+    (v: any) => !/^\s*$/.test(v) || 'No se permite espacios vacios',
+  ];
 	private FormatDate(data: any) {
 		return moment(data).format('YYYY-MM-DD');
 	}
@@ -110,7 +115,7 @@ export default class AdmPaisComponent extends Vue {
 	private Eliminar(data: services.clase_pais): void {
 		swal.fire({
 			title: 'Esta seguro de esta operacion?',
-			text: 'Eliminacion de Registro' + data.idpais,
+			text: 'Eliminacion de Registro ' + data.idpais +'/'+ data.descripcion,
 			type: 'warning',
 			showCancelButton: true,
 			confirmButtonColor: 'green',
