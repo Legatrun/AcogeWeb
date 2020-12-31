72cefde44c36d82a517ed775914df78d0e02ae12
Roger Ramos Mayta <roger.ramos@alphasys.com.bo>
2020-12-31 14:43:40 -0400

    TipoDocumentoIdentidad Validacion

--- src/components/Operaciones/TipoDocumentosIdentidad/TipoDocumentosIdentidad.ts
+++ src/components/Operaciones/TipoDocumentosIdentidad/TipoDocumentosIdentidad.ts
@@ -12,7 +12,7 @@ export default class AdmTipoDocumentosIdentidadComponent extends Vue {
 		{ text: 'IDDocumentoIdentidad', align: 'left', sortable: true, value: 'iddocumentoidentidad', width: '15%' },
 		{ text: 'descripcion', align: 'left', sortable: false, value: 'descripcion', width: '15%' },
 		{ text: 'sigla', align: 'left', sortable: false, value: 'sigla', width: '15%' },
-		{ text: 'Operaciones', align: 'center', sortable: false, value: 'action', width: '20%' },
+		{ text: 'Operaciones', align: 'left', sortable: false, value: 'action', width: '20%' },
 	];
 	private WebApi = new services.Endpoints();
 
@@ -23,6 +23,11 @@ export default class AdmTipoDocumentosIdentidadComponent extends Vue {
 	private operacion = '';
 	private helper: helpers = new helpers();
 	private popup = new popup.Swal();
+	private activo = false;
+	validacion = [
+		(v: any) => !!v || 'El campo es requerido',
+    (v: any) => !/^\s*$/.test(v) || 'No se permite espacios vacios',
+  ];
 	private FormatDate(data: any) {
 		return moment(data).format('YYYY-MM-DD');
 	}
@@ -110,7 +115,7 @@ export default class AdmTipoDocumentosIdentidadComponent extends Vue {
 	private Eliminar(data: services.clase_tipodocumentosidentidad): void {
 		swal.fire({
 			title: 'Esta seguro de esta operacion?',
-			text: 'Eliminacion de Registro' + data.iddocumentoidentidad,
+			text: 'Eliminacion de Registro ' + data.iddocumentoidentidad+'/'+ data.descripcion,
 			type: 'warning',
 			showCancelButton: true,
 			confirmButtonColor: 'green',
