0042ab795ae220bf95c570eb5fe31289f612ca91
Jorge Luis Medina Vargas <jorge.medina@alphasys.com.bo>
2021-06-24 10:48:47 -0400

    Validación sesión en pag Principal

--- src/components/Principal/Principal.ts
+++ src/components/Principal/Principal.ts
@@ -3,4 +3,9 @@ import { Component } from 'vue-property-decorator';
 
 @Component
 export default class HomeComponent extends Vue {
+    private created(){
+        if (!this.$store.state.authentication) {
+            this.$router.push({ path: '/login' });
+        }
+    }
 }
